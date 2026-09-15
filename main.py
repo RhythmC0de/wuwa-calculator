4dc61879e863077baed24c392932b195d3dc038f
Matheus <promttest18@gmail.com>
2026-09-15 00:29:31 -0300

    reorganização do codigo
    ainda falta coisa

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """Tethys launcher kept at the project root for development and packaging."""
 
-from app.main import main
+from src.app.main import main
 
 
 if __name__ == "__main__":
--- validate_data.py
+++ validate_data.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Validate character data consistency across all catalogs."""
 
-from data.characters_ids import KNOWN_CHARACTER_IDS
-from data.characters_elements import CHARACTER_ELEMENTS
-from data.characters_quotes import CHARACTER_QUOTES
-from data.images import CHARACTER_IMAGE_FALLBACKS
-from data.characters_urls import RACKOON_CHARACTER_SLUGS
-from data.weapons import _LOCAL_KIT_WEAPON_NAMES
+from src.wuwa_calculator.data.characters_ids import KNOWN_CHARACTER_IDS
+from src.data.characters_elements import CHARACTER_ELEMENTS
+from src.data.characters_quotes import CHARACTER_QUOTES
+from src.wuwa_calculator.data.images import CHARACTER_IMAGE_FALLBACKS
+from src.wuwa_calculator.data.characters_urls import RACKOON_CHARACTER_SLUGS
+from src.wuwa_calculator.data.weapons import _LOCAL_KIT_WEAPON_NAMES
 
 ids_set = KNOWN_CHARACTER_IDS
 els_set = set(CHARACTER_ELEMENTS.keys())
